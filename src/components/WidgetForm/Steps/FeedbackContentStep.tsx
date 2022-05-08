612976d02e6b2b5ee48e3646771a988b45d1bc64
MarconB11 <52143106+MarconB11@users.noreply.github.com>
2022-05-07 21:06:21 -0300

    IncludeScreenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,7 +1,8 @@
 import { Popover } from "@headlessui/react"
-import { ArrowLeft } from "phosphor-react"
+import { ArrowLeft, Camera } from "phosphor-react"
 import { FeedbackType, feedbackTypes } from ".."
 import { CloseButton } from "../../CloseButton"
+import { ScreenShotButton } from "../ScreenShotButton"
 
 interface FeedbackContentStepProps{
   feedbackType: FeedbackType
@@ -18,13 +19,13 @@ export function FeedbackContentStep({
 
   return(
     <>
-      <Popover.Button 
+      <button 
       type="button" 
       className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
       onClick={onFeedbackRestarRequested}
       >
         <ArrowLeft weight="bold" className="w-4 h-4"/>
-      </Popover.Button>
+      </button>
 
       <header>
         <span className="text-xl leading-6 flex items-center gap-2">
@@ -35,9 +36,20 @@ export function FeedbackContentStep({
       </header>
       <form className="my-4 w-full">
         <textarea
-          className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none"
+          className="min-w-[384px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
           placeholder="Conte com detalhes o que está acontecendo...."
         />
+
+        <footer className="flex gap-2 mt-2"> 
+          <ScreenShotButton/>
+
+          <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+          >
+            Enviar Feedback
+          </button>
+        </footer>
       </form>
     </>
   )
