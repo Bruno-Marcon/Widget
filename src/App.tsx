3b674efbdd35e2650064b2c587305e7a38e93e04
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2024-04-16 23:30:15 -0300

    uploading app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
 import { Widget } from './components/Widget'
+import Form from './components/table/form'
 
 export function App() {
  return(
+  <>
+  <Form/>
    <Widget/>
+  </>
  )
 }
 
